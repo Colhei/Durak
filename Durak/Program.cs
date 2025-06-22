e5ec921e248a26081ef59f03e7424c90e7aa8ca6
Ddmitro-ZH <zele_jj21@student.itstep.org>
2025-06-22 18:45:34 +0300

    v0.11

--- Durak/Program.cs
+++ Durak/Program.cs
@@ -3,45 +3,6 @@ using System.Collections.Generic;
 
 
 
-enum AllCards
-{
-    SixOfClubs = 1,
-    SevenOfClubs,
-    EightOfClubs,
-    NineOfClubs,
-    TenOfClubs,
-    JackOfClubs,
-    QueenOfClubs,
-    KingOfClubs,
-    AceOfClubs,
-    SixOfSpades,
-    SevenOfSpades,
-    EightOfSpades,
-    NineOfSpades,
-    TenOfSpades,
-    JackOfSpades,
-    QueenOfSpades,
-    KingOfSpades,
-    AceOfSpades,
-    SixOfDiamonds,
-    SevenOfDiamonds,
-    EightOfDiamonds,
-    NineOfDiamonds,
-    TenOfDiamonds,
-    JackOfDiamonds,
-    QueenOfDiamonds,
-    KingOfDiamonds,
-    AceOfDiamonds,
-    SixOfHearts,
-    SevenOfHearts,
-    EightOfHearts,
-    NineOfHearts,
-    TenOfHearts,
-    JackOfHearts,
-    QueenOfHearts,
-    KingOfHearts,
-    AceOfHearts,
-}
 
 enum CardsSuits
 {
@@ -67,52 +28,53 @@ enum HighnessOfCards
 
 class Cards
 {
-    public Dictionary<AllCards, bool> Accesibility = new Dictionary<AllCards, bool>();
-    public int Highness;
-    
-    public Cards[] AllCardsArray = new Cards[36];
+    public HighnessOfCards Highness;
+    public CardsSuits Suit;
 
-    Cards(AllCards card)
-    {
-        Accesibility[card] = true;
-    }
+    public static Cards[] AllCardsArray = new Cards[36];
+
+
 
     public static void GetCardsToArray()
     {
         int i = 0;
-        // foreach (AllCards card in Enum.GetValues(typeof(AllCards)))
+        foreach (CardsSuits suit in Enum.GetValues(typeof(CardsSuits)))
+        {
+            foreach (HighnessOfCards highness in Enum.GetValues(typeof(HighnessOfCards)))
+            {
+                AllCardsArray[i] = new Cards();
+                AllCardsArray[i].Suit = suit;
+                AllCardsArray[i].Highness = highness;
+                i++;
+            }
+        }
+
+        // foreach (Cards card in AllCardsArray)
         // {
-        //     
+        //     Console.WriteLine($"{card.Highness} of {card.Suit}");
         // }
     }
 }
 
-class Deck
-{
-    private AllCards[] MyDeck = new AllCards[6];
 
-    Deck(int[] givenCards)
-    {
-        if (givenCards.Length == 6)
-        {
-            MyDeck[0] = (AllCards)givenCards[0];
-            MyDeck[1] = (AllCards)givenCards[1];
-            MyDeck[2] = (AllCards)givenCards[2];
-            MyDeck[3] = (AllCards)givenCards[3];
-            MyDeck[4] = (AllCards)givenCards[4];
-            MyDeck[5] = (AllCards)givenCards[5];
-        }
-        else
-        {
-            throw new ArgumentException("The given array must have 6 elements.");
-        }
-    }
+
+class OtherMethods
+{
+    // static string UserInput(string input)
+    // {
+    //     
+    // }
+
+    // static Cards[] Shuffle()
+    // {
+    //     
+    // }
 }
 
-class Game
+class Program
 {
     static void Main(string[] args)
     {
-        
+        Cards.GetCardsToArray();
     }
 }
