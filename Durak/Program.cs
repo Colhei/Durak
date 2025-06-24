d622c30af86eaffeebde6c3d9f26ad05cdb5d709
Ddmitro-ZH <zele_jj21@student.itstep.org>
2025-06-24 21:44:51 +0300

    v0.13

--- Durak/Program.cs
+++ Durak/Program.cs
@@ -3,6 +3,45 @@ using System.Collections.Generic;
 
 
 
+enum AllCardsInDeck
+{
+    SixOfClubs = 1,
+    SevenOfClubs,
+    EightOfClubs,
+    NineOfClubs,
+    TenOfClubs,
+    JackOfClubs,
+    QueenOfClubs,
+    KingOfClubs,
+    AceOfClubs,
+    SixOfSpades,
+    SevenOfSpades,
+    EightOfSpades,
+    NineOfSpades,
+    TenOfSpades,
+    JackOfSpades,
+    QueenOfSpades,
+    KingOfSpades,
+    AceOfSpades,
+    SixOfDiamonds,
+    SevenOfDiamonds,
+    EightOfDiamonds,
+    NineOfDiamonds,
+    TenOfDiamonds,
+    JackOfDiamonds,
+    QueenOfDiamonds,
+    KingOfDiamonds,
+    AceOfDiamonds,
+    SixOfHearts,
+    SevenOfHearts,
+    EightOfHearts,
+    NineOfHearts,
+    TenOfHearts,
+    JackOfHearts,
+    QueenOfHearts,
+    KingOfHearts,
+    AceOfHearts,
+}
 
 enum CardsSuits
 {
@@ -32,6 +71,7 @@ class Cards
     public CardsSuits Suit;
 
     public static Cards[] AllCardsArray = new Cards[36];
+    public static Cards[] ShuffledCardDeck= new Cards[36];
 
 
 
@@ -54,27 +94,183 @@ class Cards
         //     Console.WriteLine($"{card.Highness} of {card.Suit}");
         // }
     }
+
+    public static void ShuffleCards()
+    {
+        AllCardsInDeck[] NewShuffledDeck = new AllCardsInDeck[36];
+        NewShuffledDeck = OtherMethods.Shuffle();
+        for (int i = 0; i < 36; i++)
+        {
+            while (true)
+            {
+                
+            }
+        }
+    }
 }
 
 
 
 class OtherMethods
 {
-    // static string UserInput(string input)
-    // {
-    //     
-    // }
+    private static Random rand = new Random();
+    public static (CardsSuits, HighnessOfCards) Convert(AllCardsInDeck card)
+    {
+        CardsSuits suit;
+        HighnessOfCards rank;
+        int[] Inted;
+        static int[] ConvertToInt(AllCardsInDeck card) //Конвертування з переліку всіх карт до класу Cards
+        {
+            int[] result = new int[2];
+            int Input = (int)card;
+            if (Input <= 9)
+            {
+                result[0] = 1;
+            }
+            else if (Input <= 18)
+            {
+                result[0] = 2;
+            }
+            else if (Input <= 27)
+            {
+                result[0] = 3;
+            }
+            else
+            {
+                result[0] = 4;
+            }
 
-    // static Cards[] Shuffle()
-    // {
-    //     
-    // }
+            switch (Input)
+            {
+                case 1 or 10 or 19 or 28:
+                    result[1] = 1;
+                    break;
+                case 2 or 11 or 20 or 29:
+                    result[1] = 2;
+                    break;
+                case 3 or 12 or 21 or 30:
+                    result[1] = 3;
+                    break;
+                case 4 or 13 or 22 or 31:
+                    result[1] = 4;
+                    break;
+                case 5 or 14 or 23 or 32:
+                    result[1] = 5;
+                    break;
+                case 6 or 15 or 24 or 33:
+                    result[1] = 6;
+                    break;
+                case 7 or 16 or 25 or 34:
+                    result[1] = 7;
+                    break;
+                case 8 or 17 or 26 or 35:
+                    result[1] = 8;
+                    break;
+                case 9 or 18 or 27 or 36:
+                    result[1] = 9;
+                    break;
+            }
+
+            if (result[0] != null && result[1] != null)
+            {
+                return result;
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+        }
+
+        Inted = ConvertToInt(card);
+        switch (Inted[0])
+        {
+            case 1:
+                suit = CardsSuits.Clubs;
+                break;
+            case 2:
+                suit = CardsSuits.Spades;
+                break;
+            case 3:
+                suit = CardsSuits.Diamonds;
+                break;
+            case 4:
+                suit = CardsSuits.Hearts;
+                break;
+            default:
+                throw new IndexOutOfRangeException();
+        }
+
+        switch (Inted[1])
+        {
+            case 1:
+                rank = HighnessOfCards.Six;
+                break;
+            case 2:
+                rank = HighnessOfCards.Seven;
+                break;
+            case 3:
+                rank = HighnessOfCards.Eight;
+                break;
+            case 4:
+                rank = HighnessOfCards.Nine;
+                break;
+            case 5:
+                rank = HighnessOfCards.Ten;
+                break;
+            case 6:
+                rank = HighnessOfCards.Jack;
+                break;
+            case 7:
+                rank = HighnessOfCards.Queen;
+                break;
+            case 8:
+                rank = HighnessOfCards.King;
+                break;
+            case 9:
+                rank = HighnessOfCards.Ace;
+                break;
+            default:
+                throw new IndexOutOfRangeException();
+        }
+        
+        return (suit, rank);
+    }
+    
+
+    public static AllCardsInDeck[] Shuffle()
+    {
+        static AllCardsInDeck[] CreateNewShuffledDeck()
+        {
+            AllCardsInDeck[] NewShuffledDeck = new AllCardsInDeck[36];
+            int cardToShuffle;
+            for (int i = 0; i < 36; i++)
+            {
+                while (true)
+                {
+                    cardToShuffle = rand.Next(1, 37);
+                    if (NewShuffledDeck.Contains((AllCardsInDeck)cardToShuffle)==false)
+                    {
+                        NewShuffledDeck[i] = (AllCardsInDeck)cardToShuffle;
+                        break;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+            
+            return NewShuffledDeck;
+        }
+        return CreateNewShuffledDeck();
+    }
 }
 
 class Program
 {
     static void Main(string[] args)
     {
-        Cards.GetCardsToArray();
+        
     }
 }
