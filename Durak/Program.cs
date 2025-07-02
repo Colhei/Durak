f5bd01a041c7e2cd6df3a9e6123fc0c680f44a05
Colhei <zelezka2413@gmail.com>
2025-07-02 23:52:36 +0300

    v0.3

--- Durak/Program.cs
+++ Durak/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Data.SqlTypes;
 
 
 enum AllCardsInDeck // перелік усіх кард та їх нумерація
@@ -14,7 +14,7 @@ enum AllCardsInDeck // перелік усіх кард та їх нумерац
     QueenOfClubs,
     KingOfClubs,
     AceOfClubs,
-    SixOfSpades, // індекс - 10
+    SixOfSpades,
     SevenOfSpades, 
     EightOfSpades,
     NineOfSpades,
@@ -23,7 +23,7 @@ enum AllCardsInDeck // перелік усіх кард та їх нумерац
     QueenOfSpades,
     KingOfSpades,
     AceOfSpades,
-    SixOfDiamonds, // індекс - 19
+    SixOfDiamonds,
     SevenOfDiamonds,
     EightOfDiamonds,
     NineOfDiamonds,
@@ -32,7 +32,7 @@ enum AllCardsInDeck // перелік усіх кард та їх нумерац
     QueenOfDiamonds,
     KingOfDiamonds,
     AceOfDiamonds,
-    SixOfHearts, // індекс - 28
+    SixOfHearts,
     SevenOfHearts,
     EightOfHearts,
     NineOfHearts,
@@ -45,7 +45,7 @@ enum AllCardsInDeck // перелік усіх кард та їх нумерац
 
 enum CardsSuits // масті карт
 {
-    Clubs = 1,
+    Clubs,
     Spades,
     Diamonds,
     Hearts,
@@ -69,7 +69,7 @@ class Cards // клас "карти"
 {
     public HighnessOfCards Highness;
     public CardsSuits Suit;
-    private bool CardAccessibility = true;
+    private bool CardAvailable = true;
 
     public static Cards[] AllCardsArray = new Cards[36]; // масив з усіма картами у вигляді об'єктів класу
 
@@ -101,10 +101,9 @@ class Cards // клас "карти"
         foreach (var card in AllCardsArray)
         {
             (CardsSuits, HighnessOfCards) Card = OtherMethods.Convert(NewShuffledDeck[iter]);
-            // Console.WriteLine($"{Card.Item1} {Card.Item2}");
+            Console.WriteLine($"{Card.Item1} {Card.Item2}");
             
-            // ShuffledDeck[iter].Suit = Card.Item1;
-            // ShuffledDeck[iter].Highness = Card.Item2;
+            
 
             card.Suit = Card.Item1;
             card.Highness = Card.Item2;
@@ -119,7 +118,7 @@ class Cards // клас "карти"
 
 
 
-class OtherMethods // клас з усіма іншими методами
+class OtherMethods // усі інші методами
 {
     private static Random rand = new Random();
     public static (CardsSuits, HighnessOfCards) Convert(AllCardsInDeck card) // метод конвертації з обєкту переліку AllCardsInDeck
@@ -273,6 +272,265 @@ class OtherMethods // клас з усіма іншими методами
         }
         return CreateNewShuffledDeck();
     }
+
+    public static AllCardsInDeck ConvertToCard(HighnessOfCards highness, CardsSuits suit)
+    {
+        try
+        {
+            int cardIndex;
+            int rankIndex = (int)highness;
+            int suitIndex = (int)suit;
+            AllCardsInDeck result;
+            cardIndex = rankIndex + (suitIndex * 9);
+            result = (AllCardsInDeck)cardIndex;
+            Console.WriteLine($"{cardIndex} \n {result}");
+            return result;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Виникла помилка у обробці входу --> {e.Message}");
+            return default;
+        }
+    }
+    
+    public static List<AllCardsInDeck> PlayerInputConv(string input) 
+    {
+        try
+        {
+            List<AllCardsInDeck> result = new List<AllCardsInDeck>();
+            (CardsSuits, HighnessOfCards) card;
+            (CardsSuits, HighnessOfCards) card2;
+            string[] playerInp = input.Split(" ");
+            if (playerInp[0] == "six" || playerInp[0] == "6")
+            {
+                card.Item2 = HighnessOfCards.Six;
+            }else if (playerInp[0] == "seven" || playerInp[0] == "7")
+            {
+                card.Item2 = HighnessOfCards.Seven;
+            }else if (playerInp[0] == "eight" || playerInp[0] == "8")
+            {
+                card.Item2 = HighnessOfCards.Eight;
+            }else if (playerInp[0] == "nine" || playerInp[0] == "9")
+            {
+                card.Item2 = HighnessOfCards.Nine;
+            }else if (playerInp[0] == "ten" || playerInp[0] == "10")
+            {
+                card.Item2 = HighnessOfCards.Ten;
+            }else if (playerInp[0] == "jack")
+            {
+                card.Item2 = HighnessOfCards.Jack;
+            }else if (playerInp[0] == "queen")
+            {
+                card.Item2 = HighnessOfCards.Queen;
+            }else if (playerInp[0] == "king")
+            {
+                card.Item2 = HighnessOfCards.King;
+            }else if (playerInp[0] == "ace")
+            {
+                card.Item2 = HighnessOfCards.Ace;
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            if (playerInp[2] == "spades")
+            {
+                card.Item1 = CardsSuits.Spades;
+            }else if (playerInp[2] == "diamonds")
+            {
+                card.Item1 = CardsSuits.Diamonds;
+            }else if (playerInp[2] == "hearts")
+            {
+                card.Item1 = CardsSuits.Hearts;
+            }else if (playerInp[2] == "clubs")
+            {
+                card.Item1 = CardsSuits.Clubs;
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
+            }
+            
+            if (playerInp[3] == "six" || playerInp[3] == "6")
+            {
+                card2.Item2 = HighnessOfCards.Six;
+            }else if (playerInp[3] == "seven" || playerInp[3] == "7")
+            {
+                card2.Item2 = HighnessOfCards.Seven;
+            }else if (playerInp[3] == "eight" || playerInp[3] == "8")
+            {
+                card2.Item2 = HighnessOfCards.Eight;
+            }else if (playerInp[3] == "nine" || playerInp[3] == "9")
+            {
+                card2.Item2 = HighnessOfCards.Nine;
+            }else if (playerInp[3] == "ten" || playerInp[3] == "10")
+            {
+                card2.Item2 = HighnessOfCards.Ten;
+            }else if (playerInp[3] == "jack")
+            {
+                card2.Item2 = HighnessOfCards.Jack;
+            }else if (playerInp[3] == "queen")
+            {
+                card2.Item2 = HighnessOfCards.Queen;
+            }else if (playerInp[3] == "king")
+            {
+                card2.Item2 = HighnessOfCards.King;
+            }else if (playerInp[3] == "ace")
+            {
+                card2.Item2 = HighnessOfCards.Ace;
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            if (playerInp[5] == "spades")
+            {
+                card2.Item1 = CardsSuits.Spades;
+            }else if (playerInp[5] == "diamonds")
+            {
+                card2.Item1 = CardsSuits.Diamonds;
+            }else if (playerInp[5] == "hearts")
+            {
+                card2.Item1 = CardsSuits.Hearts;
+            }else if (playerInp[5] == "clubs")
+            {
+                card2.Item1 = CardsSuits.Clubs;
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
+            }
+            
+            
+            
+            return result;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"помилка у обробці входу -> {e.Message}");
+            Game.GameInProcess = false;
+            return null;
+        }
+    }
+    
+}
+
+class Player
+{
+    private AllCardsInDeck[] PlayerDeck = new AllCardsInDeck[6];
+    public string PlayerName;
+    const int MaxCards = 6;
+
+    public Player(string playerName)
+    {
+        PlayerName = playerName;
+    }
+
+    int CountCards()
+    {
+        int iter = 0;
+        int TotalCards = 0;
+        Console.WriteLine(PlayerDeck[0]);
+        foreach (var card in PlayerDeck)
+        {
+            if (card != null && card != default)
+            {
+                TotalCards++;
+            }
+            iter++;
+        }
+        return TotalCards;
+    }
+
+    void GetCards()
+    {
+        if (CountCards() < MaxCards)
+        {
+            foreach (var card in PlayerDeck)
+            {
+                if (card != null && card != default)
+                {
+                    
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
+
+class Game
+{
+    public static int PlayerAmount = 4;
+    public static bool GameInProcess = true;
+
+    static (CardsSuits, HighnessOfCards) PlayerInput(string input)
+    {
+        try
+        {
+            (CardsSuits, HighnessOfCards) ReturnArr = (default, default);
+            string[] Parts = input.Split(' ');
+            // місце для перевірки вірності вводу
+
+            switch (Parts[0]) // встановлення старшинства карти
+            {
+                case "6":
+                    ReturnArr.Item2 = HighnessOfCards.Six;
+                    break;
+                case "7":
+                    ReturnArr.Item2 = HighnessOfCards.Seven;
+                    break;
+                case "8":
+                    ReturnArr.Item2 = HighnessOfCards.Eight;
+                    break;
+                case "9":
+                    ReturnArr.Item2 = HighnessOfCards.Nine;
+                    break;
+                case "10":
+                    ReturnArr.Item2 = HighnessOfCards.Ten;
+                    break;
+                case "Jack":
+                    ReturnArr.Item2 = HighnessOfCards.Jack;
+                    break;
+                case "Queen":
+                    ReturnArr.Item2 = HighnessOfCards.Queen;
+                    break;
+                case "King":
+                    ReturnArr.Item2 = HighnessOfCards.King;
+                    break;
+                case "Ace":
+                    ReturnArr.Item2 = HighnessOfCards.Ace;
+                    break;
+            }
+
+            switch (Parts[2])
+            {
+                case "clubs":
+                    ReturnArr.Item1 = CardsSuits.Clubs;
+                    break;
+                case "spades":
+                    ReturnArr.Item1 = CardsSuits.Spades;
+                    break;
+                case "diamonds":
+                    ReturnArr.Item1 = CardsSuits.Diamonds;
+                    break;
+                case "hearts":
+                    ReturnArr.Item1 = CardsSuits.Hearts;
+                    break;
+            }
+            return ReturnArr;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Виникла помилка {e}: {e.Message}");
+            GameInProcess = false;
+        }
+        return default;
+    }
 }
 
 class Program
@@ -282,3 +540,15 @@ class Program
         
     }
 }
+
+/*
+ *
+ *
+ *          ввід:
+ * 
+ *          ввід карти: *старшинство* of *масть*    приклад  -->   six of spades (6 of Spades) = шість пікових,   queen of hearts = чирвова дама
+ *          ввід ходу:     *карта яку бити* *карта якою бити*   приклад -->   six of clubs ten of clubs = побити шістку хрестову десфткою хрестовою
+ *
+ * 
+ */
+
